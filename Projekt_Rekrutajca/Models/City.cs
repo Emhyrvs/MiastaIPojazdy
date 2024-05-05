74a6fe778a62b54e59e929ab02e8da56b6c421c6
maciej.zakrzewski <maciej.zakrzewski@bcoders.pl>
2024-05-05 12:27:30 +0200

    Second fix

--- Projekt_Rekrutajca/Models/City.cs
+++ Projekt_Rekrutajca/Models/City.cs
@@ -9,7 +9,7 @@ namespace Projekt_Rekrutajca.Models
         [Required]
         public string Name { get; set; }
         [Required]
-        [Range(1,1000)]
+        [Range(1,1000000000)]
         public int Population { get; set; }
     }
 }
